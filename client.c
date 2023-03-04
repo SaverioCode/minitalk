1c2f7b89413a49c7b3132c0c76693159e7e77cab
Francesco Saverio Garzi Cosentino <garzicosentino@gmail.com>
2023-03-04 21:21:57 +0100

    update

--- client.c
+++ client.c
@@ -6,31 +6,32 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 01:11:18 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/03/04 02:14:49 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/03/04 21:08:48 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib_minitalk.h"
 
+void	ft_recive(int signo)
+{
+	write(1, "Message successfully sended!\n", 29);
+	signo = 0;  ///////
+}
+
 void	ft_sender(int pid, char *str)
 {
-	int	offset;
+	short int	offset;
 
-	// this goes to the server part
-	// if (str[0] = 37 && str[1] == 64 && !str[2])
-	// {
-	// 	write(1, "Bye!\n", 5);
-	// 	exit(0);
-	// }
 	while (*str)
 	{
-		offset = -1;
-		while (++offset < 8)
+		offset = 8;
+		while (--offset >= 0)
 		{
 			if ((1 << offset) & *str)
-				kill(pid, SIGUSR1);
-			else
 				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			usleep(500);
 		}
 		str++;
 	}
@@ -44,6 +45,7 @@ int	main(int ac, char **av)
 		return (0);
 	}
 	ft_sender(ft_atoi(av[1]), av[2]);
+	signal(SIGUSR2, ft_recive);
 	return (0);
 }
 
--- server.c
+++ server.c
@@ -6,15 +6,48 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 01:08:52 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/03/04 02:10:13 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/03/04 21:19:17 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib_minitalk.h"
 
+int	ft_handler(int signo, siginfo_t *info, void *context)
+{
+	static char	c;
+	static int	i;
+
+	printf("i: %d	c: %c	(int)c: %d\n", i, c, (int)c);
+	if (signo == SIGUSR2)
+	{
+		write(1, "signo\n", 6);
+		c |= 1 << i;
+		printf("%c %d\n", c, (int)c);
+	}
+	if (++i == 8)
+	{
+		if (!c)
+			exit(0);
+		write(1, &c, 1);
+		kill(info -> si_pid, SIGUSR2);
+		i = 0;
+		c = 0;
+	}
+	write(1, "TEST\n", 5);
+	(void)context;
+	return (0);
+}
+
 int	main(void)
 {
+	struct sigaction	sa;
+
 	ft_printf("%d\n", getpid());
-	pause();
+	// sa = NULL;
+	sa.sa_handler = (void *)ft_handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
 	return (0);
 }
